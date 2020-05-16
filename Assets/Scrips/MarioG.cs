986f16500d5431f6da2bacc8ae6e216768ba7727
MadeColombia <MadeColombia@users.noreply.github.com>
2020-05-16 11:27:00 -0500

    Actualización
    En esta actualización esta el personaje junto a sus animaciones. Tambien se eliminaron los planetas de la escena

---
+++ Assets/Scrips/MarioG.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioG : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] planetas;
+    void Start()
+    {
+        planetas[7] = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.gravity = planetas[0].transform.position - transform.position;
+        transform.rotation = Quaternion.FromToRotation(transform.up, -Physics.gravity) * transform.rotation;
+    }
+}
